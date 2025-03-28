d7deb06f5dbaaaf6a7e7d795567ad44c5fb5fc3e
Oliver Lorenz <mail@oliverlorenz.com>
2025-03-28 21:41:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# <REPO_NAME>
+
+a description
+
+## Installation
+
+To install <REPO_NAME>, use npm:
+
+```bash
+npm install @oliverlorenz/<REPO_NAME>
+```
---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+.tsx?$": ["ts-jest", {}],
+  },
+  rootDir: "./src",
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+  coverageDirectory: "../coverage",
+};
